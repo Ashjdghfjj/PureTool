14501f1b7074948239f64324654c60c7ad441d7d
Ashjdghfjj <aabcdefgh202301@163.com>
2026-02-07 19:03:39 +0800

    feat: add chrome extension support

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import './i18n'; // Import i18n configuration
 import { ThemeProvider } from './components/ThemeProvider.tsx'
 
+// Check if running as a Chrome Extension
+const isExtension = window.location.protocol === 'chrome-extension:' || window.location.protocol === 'file:';
+const Router = isExtension ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 )
